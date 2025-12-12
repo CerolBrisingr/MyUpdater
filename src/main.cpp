7c2316a24aa0a79ca1f42616ae7413107768a41d
CerolBrisingr <73132285+CerolBrisingr@users.noreply.github.com>
2025-12-12 14:51:55 +0100

    Moving into github

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Updater2/IO/filesystem.h"
+
+#include <iostream>
+
+using namespace Updater2;
+
+int main()
+{
+    int test{ 10 };
+    std::cout << "Hello world!\n";
+    IO::printCurrentPath();
+
+    return 0;
+}
