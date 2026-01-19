d505872ec7458773cf108d3f36b0c92eed598044
CerolBrisingr <73132285+CerolBrisingr@users.noreply.github.com>
2026-01-19 15:02:13 +0100

    Fixed variable renaming going too far fileinteraction_test was affected as well

--- libs/io/tests/fileinteraction/fileinteraction_test.cpp
+++ libs/io/tests/fileinteraction/fileinteraction_test.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 
-namespace ssl = Updater2::IO;
+namespace myfs = Updater2::IO;
 
 TEST(FilesystemTest, Bonus) {
 	EXPECT_TRUE(nullptr == NULL);
@@ -12,16 +12,16 @@ TEST(FilesystemTest, Bonus) {
 
 // Demonstrate some basic assertions.
 TEST(FilesystemTest, BasicAssertions) {
-	EXPECT_TRUE(ssl::isFolder("../md5"));
-	EXPECT_TRUE(ssl::isFile("md5_result.txt"));
+	EXPECT_TRUE(myfs::isFolder("../md5"));
+	EXPECT_TRUE(myfs::isFile("md5_result.txt"));
 }
 
 TEST(FilesystemTest, Md5) {
-	auto md5Calc{ ssl::calculateMd5Hash("md5_target.txt") };
-	EXPECT_FALSE(ssl::compareMd5Hashes(md5Calc, ""));
-	auto md5Calc2{ ssl::calculateMd5Hash("md5_target2.txt") };
-	EXPECT_FALSE(ssl::compareMd5Hashes(md5Calc2, md5Calc));
-	auto md5Read{ ssl::readFirstLineInFile("md5_result.txt") };
-	EXPECT_FALSE(ssl::compareMd5Hashes(md5Read, ""));
-	EXPECT_TRUE(ssl::compareMd5Hashes(md5Calc, md5Read));
+	auto md5Calc{ myfs::calculateMd5Hash("md5_target.txt") };
+	EXPECT_FALSE(myfs::compareMd5Hashes(md5Calc, ""));
+	auto md5Calc2{ myfs::calculateMd5Hash("md5_target2.txt") };
+	EXPECT_FALSE(myfs::compareMd5Hashes(md5Calc2, md5Calc));
+	auto md5Read{ myfs::readFirstLineInFile("md5_result.txt") };
+	EXPECT_FALSE(myfs::compareMd5Hashes(md5Read, ""));
+	EXPECT_TRUE(myfs::compareMd5Hashes(md5Calc, md5Read));
 }
