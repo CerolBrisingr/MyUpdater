5c3486e0b951faa4eaf63aa6c65ec97763c0f733
CerolBrisingr <73132285+CerolBrisingr@users.noreply.github.com>
2026-02-16 17:43:26 +0100

    Cleaned up launcher function for windows
    Tied input encoding to utf-8 while at it.

--- libs/io/src/fileinteractions_windows.cpp
+++ libs/io/src/fileinteractions_windows.cpp
@@ -3,70 +3,62 @@
 
 namespace Updater2::IO {
 
+    namespace {
+        std::wstring getQuotedArgument(const std::string& arg) {
+            // We fully expect the string to be encoded with utf-8
+            std::u8string u8Arg(arg.begin(), arg.end());
+            // use string translation in std::filesystem::path
+            std::wstring wArg = std::filesystem::path(u8Arg).wstring();
+            if (wArg.find(L'"') == std::wstring::npos) {
+                // Argument contains no quotes, add some
+                wArg = L"\"" + wArg + L"\"";
+            }
+            return wArg;
+        }
+    } // namespace
+
 
     bool createProcess(const std::filesystem::path& executable, const stringList& args) {
-        // 1. Kommandozeile für Windows bauen
+        // Build command line ("executable" "arg1" "arg2" ...)
         std::wstring cmd = L"\"" + executable.wstring() + L"\"";
         for (const auto& arg : args) {
-            std::wstring wArg = std::filesystem::path(arg).wstring();
-            if (wArg.find(L' ') != std::wstring::npos) wArg = L"\"" + wArg + L"\"";
-            cmd += L" " + wArg;
+            cmd += L" " + getQuotedArgument(arg);
         }
 
-        STARTUPINFOW si = { sizeof(si) };
+        STARTUPINFOW si = { 
+            .cb = sizeof(si),
+            .dwFlags = STARTF_USESHOWWINDOW,
+            .wShowWindow = SW_SHOWDEFAULT
+        };
         PROCESS_INFORMATION pi = { 0 };
+
+        // Adjust format for CreateProcessW (needs to be able to modify content)
         std::vector<wchar_t> buffer(cmd.begin(), cmd.end());
         buffer.push_back(0);
 
-        if (CreateProcessW(NULL, buffer.data(), NULL, NULL, FALSE,
-                           DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP,
-                           NULL, NULL, &si, &pi)) {
+        std::wstring workingDir = executable.parent_path().wstring();
+
+        if (CreateProcessW(
+            NULL,                   // No module name (use command line)
+            buffer.data(),          // Command line
+            NULL,                   // Process handle not inheritable
+            NULL,                   // Thread handle not inheritable
+            FALSE,                  // Set handle inheritance to FALSE
+            DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP, // Creation flags
+            NULL,                   // Use parent's environment block
+            workingDir.c_str(),     // Use target's starting directory 
+            &si,                    // Pointer to STARTUPINFO structure
+            &pi                     // Pointer to PROCESS_INFORMATION structure
+        )) {
+            // Close process and thread handles. Don't leak process control.
             CloseHandle(pi.hProcess);
             CloseHandle(pi.hThread);
-            return true; // Hauptprogramm läuft hier einfach weiter
+            return true;
                            }
-        return false;
-    }
-
-    // TODO: remove
-    bool createProcess(const std::filesystem::path& processPath, const std::wstring& commandLineArgs) {
-
-        STARTUPINFOW si{ sizeof(si) };
-        PROCESS_INFORMATION pi{ 0 };
-
-        si.dwFlags = STARTF_USESHOWWINDOW;
-        si.wShowWindow = SW_SHOWDEFAULT;
-
-        // To avoid errors: set binary path in quotes
-        std::wstring fullCmd = L"\"" + processPath.wstring() + L"\"";
-        // Add command line arguments
-        if (!commandLineArgs.empty()) {
-            fullCmd += L" " + commandLineArgs;
-        }
-
-        std::wstring workingDir = processPath.parent_path().wstring();
-
-        // Start the child process. 
-        if (!CreateProcessW(NULL,   // No module name (use command line)
-            fullCmd.data(),         // Command line
-            NULL,           // Process handle not inheritable
-            NULL,           // Thread handle not inheritable
-            FALSE,          // Set handle inheritance to FALSE
-            DETACHED_PROCESS,       // Creation flags
-            NULL,           // Use parent's environment block
-            workingDir.c_str(),     // Use target's starting directory 
-            &si,     // Pointer to STARTUPINFO structure
-            &pi)     // Pointer to PROCESS_INFORMATION structure
-            )
-        {
+        else {
+            // GetLastError() ...
             return false;
         }
-
-        // Close process and thread handles. Don't leak process control.
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-
-        return true;
     }
 
 } // namespace Updater2::IO
