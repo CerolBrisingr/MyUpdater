2ab8f410299d2f3872f48b14e870be5268b3a89a
CerolBrisingr <73132285+CerolBrisingr@users.noreply.github.com>
2026-02-17 12:57:26 +0100

    Adjusted comments for linux launcher

--- libs/io/src/fileinteractions_linux.cpp
+++ libs/io/src/fileinteractions_linux.cpp
@@ -5,7 +5,7 @@ namespace Updater2::IO {
 
     bool createProcess(const std::filesystem::path& executable, const stringList& args) {
 
-        // Argumente vorbereiten
+        // Prepare full argument vector
         std::vector<std::string> sArgs;
         sArgs.push_back(executable.string());
         for (const auto& a : args) sArgs.push_back(a);
@@ -14,17 +14,17 @@ namespace Updater2::IO {
         for (auto& s : sArgs) cArgs.push_back(const_cast<char*>(s.c_str()));
         cArgs.push_back(nullptr);
 
-        // Pfad extrahieren, bevor wir forken
+        // Extract path for visibility
         const char* path = cArgs[0];
 
-        // Linux: Klassisch fork/exec
+        // Fork/exec, as slim as we can make it
         pid_t pid = fork();
-        if (pid < 0) return false;
-        if (pid > 0) return true; // Elternprozess
+        if (pid < 0) return false; // Something went wrong
+        if (pid > 0) return true; // Parent process
 
-        setsid();
+        setsid(); // Own session for fork
 
-        // Standard-IO trennen
+        // Seperate standard-IO
         close(0); close(1); close(2);
 
         execv(path, cArgs.data());
